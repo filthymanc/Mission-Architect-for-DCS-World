c938b79af3d387ef8fe18c8be71f97be27983d58
filthymanc <filthymanc@gmail.com>
2026-02-12 00:00:20 +0000

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Mission-Architect-for-DCS-World/sw.js', { scope: '/Mission-Architect-for-DCS-World/' })})}
